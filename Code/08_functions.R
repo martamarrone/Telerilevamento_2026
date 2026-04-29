ce570bc558d8c899f9ecf132119cd082f19ef986
mar-tor-e <marta.marrone2@studio.unibo.it>
2026-04-29 17:04:13 +0200

    Update 08_functions.R

--- Code/08_functions.R
+++ Code/08_functions.R
@@ -18,3 +18,50 @@ url = "link"
 qr = qr_code(url)
 png("github_profile_qr", width = 1000, height = 1000)
 plot(qr)
+
+#cambiare nome a funzioni
+mf =function(nx = 1, ny = 2) {
+  par(mfrow=c(nx,ny))
+  }
+#nx = 1 e ny = 2 sono i valori di default
+#quindi se so che ho bisogno di fare tante volte questo confronto posso impostare un default e quindi se scrivo solo mf() usa i valori di default
+#se non metto =1 e =2 allora non ha valori di default
+
+numeri = function(x) {
+  if(x>0) {
+  print("Questo numero è positivo")
+    }
+  else {
+   print("Questo numero è negativo")
+    }
+  }
+#per considerare anche lo 0
+numeri = function(x) {
+  if(x>0) {
+  print("Questo numero è positivo")
+    }
+  else if(x<0) {
+  print("Questo numero è negativo")
+    }
+  else {
+  print("È zero")
+    }
+  }
+
+#ciclo for
+#iterazione, quando devo fare lo stesso calcolo per tanti dati
+#l'oggetto i potrei chiamarlo come mi pare
+
+loop = function() {
+  for(i in 1:5) {
+    print(i)
+    }
+  }
+
+loop2 = function() {
+  for(i in 1:15) {
+    op = 
+    print(op)
+    }
+  }
+#prende tutte le i e moltiplica per 3 
